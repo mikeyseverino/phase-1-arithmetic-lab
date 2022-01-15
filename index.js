f1508408b18a0d9eb9a9b1bdf365af55309073bb
Michael Severino <mikeyseverino@gmail.com>
2022-01-15 15:58:12 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+//Multiply
 
-// Write your code here
+let multiply;
+    
+const num1 = 2;
+
+const num2 = 31;
+
+multiply = num1*num2;
+  
+//Random
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+//Mod
+
+let mod;
+
+const num3 = 16;
+
+const num4 = 4;
+
+mod = num4%num3;
+
+//Max
+
+const max = Math.max(5, 10, 15, 20, 1);
